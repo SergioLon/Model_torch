660f7d00a67ec63b0623bc6952964ad1fa74161f
SergioLon <77145026+SergioLon@users.noreply.github.com>
2021-02-13 16:17:35 +0100

    forgot training code

--- code_for_the_whole_dataset/training_code.py
+++ code_for_the_whole_dataset/training_code.py
@@ -1,10 +1,11 @@
 import numpy as np
-from losses import nmse,NMAE
+from losses import nmse,NMAE,Cos_sim
 import torch
 def training(hyperParams,model,data_loaders,optimizer,scheduler):
     saved_loss=np.zeros((2,hyperParams["epochs"]),dtype=np.dtype('float32'))
     train_metr=np.zeros((3,hyperParams["epochs"]),dtype=np.dtype('float32'))
     val_metr=np.zeros((3,hyperParams["epochs"]),dtype=np.dtype('float32'))
+    cosine_simil=np.zeros((2,hyperParams["epochs"]),dtype=np.dtype('float32'))
     print('-'*40+'\nTRAINING  STARTED\n'+'-'*40)
     for epoch in range(hyperParams['epochs']):
         try:
@@ -12,6 +13,8 @@ def training(hyperParams,model,data_loaders,optimizer,scheduler):
             print(f'Epoch: {epoch:3d}')
             train_loss=0.0
             val_loss=0.0
+            cos_train_loss=0.0
+            cos_val_loss=0.0
             train_metric=np.zeros((3,1),dtype=float)
             val_metric=np.zeros((3,1),dtype=float)
             for phase in ['train', 'val']:
@@ -38,6 +41,7 @@ def training(hyperParams,model,data_loaders,optimizer,scheduler):
                     loss = nmse(out, batch.norm)  # Compute the loss solely based on the training nodes.
                     nmae=NMAE(out,batch.norm)
                     
+                    cos_loss=Cos_sim(out,batch.norm)
                     # loss = nmse(out, batch.wss_coord)  # Compute the loss solely based on the training nodes.
                     # nmae=NMAE(out,batch.wss_coord)
                     #print(out.size())
@@ -47,14 +51,17 @@ def training(hyperParams,model,data_loaders,optimizer,scheduler):
                     optimizer.zero_grad()
                     if phase == 'train':
                         loss.backward()
+                        cos_loss.backward()
                         #torch.sum(nmae).backward()
                         # update the weights
                         optimizer.step()
                         train_loss+=loss.data
                         train_metric=train_metric+nmae.detach().numpy()
+                        cos_train_loss+=cos_loss
                     else:
                         val_loss+=loss.data
                         val_metric=val_metric+nmae.detach().numpy()
+                        cos_val_loss+=cos_loss
                         scheduler.step(val_loss)
         
             
@@ -62,16 +69,21 @@ def training(hyperParams,model,data_loaders,optimizer,scheduler):
             val_loss /=len(data_loaders['val'])
             train_metric /= len(data_loaders['train'])
             val_metric /=len(data_loaders['val'])
-            
+            cos_train_loss /= len(data_loaders['train'])
+            cos_val_loss /=len(data_loaders['val'])
             #save the loss
             saved_loss[0][epoch]=train_loss
             saved_loss[1][epoch]=val_loss
+            # Cosine similarity
+            cosine_simil[0][epoch]=cos_train_loss
+            cosine_simil[1][epoch]=cos_val_loss
             for j in range(3):
                 train_metr[j][epoch]=train_metric[j]
                 val_metr[j][epoch]=val_metric[j]
             #print loss for each epoch
+            print('{} COSINE SIMILARITY: {:.4f}; {} COSINE SIMILARITY: {:.4f}'.format('Train', cos_train_loss,'Val',cos_val_loss))
             print('{} NMSE: {:.4f}; {} NMSE: {:.4f}'.format('Train', train_loss,'Val',val_loss))
             print('{} NMAE: {:.4f}; {} NMAE: {:.4f}'.format('Train', np.sum(train_metric),'Val',np.sum(val_metric)))
         except KeyboardInterrupt:
             break
-    return model,saved_loss,train_metr,val_metr
+    return model,saved_loss,train_metr,val_metr,cosine_simil
