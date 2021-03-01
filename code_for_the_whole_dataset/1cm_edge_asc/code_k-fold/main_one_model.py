283eb42a79b9974cd8960181d16dd8a9ceb2c994
SergioLon <77145026+SergioLon@users.noreply.github.com>
2021-03-01 16:59:25 +0100

    back to init

--- code_for_the_whole_dataset/1cm_edge_asc/code_k-fold/main_one_model.py
+++ code_for_the_whole_dataset/1cm_edge_asc/code_k-fold/main_one_model.py
@@ -47,15 +47,19 @@ def weights_init_uniform_rule(m):
         # for every Linear layer in a model..
         if classname.find('FeaStConv') != -1:
             # get the number of the inputs
-            n = m.in_channels
-            y = 1.0/np.sqrt(n)
-            m.weight.data.uniform_(-y, y)
+            # n = m.in_channels
+            # y = 1.0/np.sqrt(n)
+            #m.weight.data.uniform_(-y, y)
+            torch.nn.init.normal_(m.weight,mean=0,std=0.3)
+            torch.nn.init.zeros_(m.bias)
             #m.bias.data.fill_(0)
         if classname.find('Linear') != -1:
             # get the number of the inputs
-            n = m.in_features
-            y = 1.0/np.sqrt(n)
-            m.weight.data.uniform_(-y, y)
+            # n = m.in_features
+            # y = 1.0/np.sqrt(n)
+            # m.weight.data.uniform_(-y, y)
+            torch.nn.init.normal_(m.weight,mean=0,std=0.3)
+            torch.nn.init.zeros_(m.bias)
     # create a new model with these weights
 model = GCN()
 
