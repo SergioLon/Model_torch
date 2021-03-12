ae2a95d4cb02a535d30d913df269763b500e160a
SergioLon <77145026+SergioLon@users.noreply.github.com>
2021-03-12 10:35:42 +0100

    new code only for abs

---
+++ code_for_results/MyOwnDataset_normalize_val.py
@@ -0,0 +1,390 @@
+import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+import glob
+import pyvista as pv
+import numpy as np
+#import trimesh as tr
+
+#import matplotlib.pyplot as plt
+import torch
+from MyOwnDataset_normalize_train import MyOwnDataset_normalize_train
+from torch_geometric.transforms import Compose,KNNGraph,FaceToEdge,GenerateMeshNormals
+from new_random_rotate import RandomRotate
+from torch_geometric.data import Data,DataLoader,InMemoryDataset
+
+class Normilize_WSS(object):
+    def __call__(self,data):
+        
+        maxm = data.wss_coord.max(dim=-2).values
+        minm = data.wss_coord.min(dim=-2).values
+        
+        data.wss_min[:]=minm.min()
+        data.wss_max[:]=minm.max()
+        print("OLD WSS MAX: ",maxm)
+        print("OLD WSS MIN: ",minm)
+        # print("OLD POS_X MAX: ",data.pos_x.max())
+        # print("OLD POS_X MIN: ",data.pos_x.min())
+        #mean = ( maxm.max() + minm.min() ) / 2.
+        ## NORMALIZE [-1,1]
+        maxm_abs = data.wss_coord.abs().max(dim=-2).values
+        
+        data.wss_max_abs[:]=maxm_abs.max()
+        #data.wss_coord = (data.wss_coord)/maxm_abs.max()
+        ## NORMALIZE [0,1]
+        #data.wss_coord = (data.wss_coord - minm.min()) / ( (maxm.max() - minm.min()))
+        
+        ## NORMALIZE STD=1
+        ##LOAD TRAINING MEAN AND STD
+        dataset=MyOwnDataset_normalize_train(root='dataset/training',)
+        train_loader = DataLoader(dataset, batch_size=1)
+        for batch in train_loader:
+            std_abs=batch.wss_std_abs
+            wss_abs_mean=batch.wss_mean_abs
+            
+            std_x=batch.wss_std_x
+            std_y=batch.wss_std_y
+            std_z=batch.wss_std_z
+            wss_mean_x = batch.wss_mean_x
+            wss_mean_y = batch.wss_mean_y
+            wss_mean_z = batch.wss_mean_z
+            break
+        data.wss_std_x[:]=std_x
+        data.wss_std_y[:]=std_y
+        data.wss_std_z[:]=std_z
+        
+        data.wss_std_abs[:]=std_abs
+        data.wss_mean_abs[:]=wss_abs_mean
+        
+        data.wss_mean_x[:]=wss_mean_x
+        data.wss_mean_y[:]=wss_mean_y
+        data.wss_mean_z[:]=wss_mean_z
+        print("WSS ABS MEAN: ",wss_abs_mean)
+        print("WSS ABS STD: ",std_abs)
+        print("WSS MEAN: ",wss_mean_x,wss_mean_y,wss_mean_z)
+        print("WSS STD X: ",std_x)
+        print("WSS STD Y: ",std_y)
+        print("WSS STD Z: ",std_z)
+        data.wss_coord[:,0] = (data.wss_coord[:,0]-wss_mean_x) /std_x
+        data.wss_coord[:,1] = (data.wss_coord[:,1]-wss_mean_y) /std_y
+        data.wss_coord[:,2] = (data.wss_coord[:,2]-wss_mean_z) /std_z
+        maxm = data.wss_coord.max(dim=-2).values
+        minm = data.wss_coord.min(dim=-2).values
+        wss_mean = ( maxm + minm ) / 2.
+        
+        std_x=torch.std(data.wss_coord[:,0])
+        std_y=torch.std(data.wss_coord[:,1])
+        std_z=torch.std(data.wss_coord[:,2])
+        # print("NEW MEAN X: ",mean_x)
+        # print("NEW MEAN Y: ",mean_y)
+        # print("NEW MEAN Z: ",mean_z)
+        data.wss_abs=(data.wss_abs-wss_abs_mean)/std_abs
+        wss_abs_mean=torch.mean(data.wss_abs)
+        std_abs=torch.std(data.wss_abs)
+        print("NEW WSS ABS MEAN: ",wss_abs_mean)
+        print("NEW WSS ABS STD : ",std_abs)
+        
+        print("NEW WSS MEAN: ",wss_mean)
+        print("NEW WSS STD X: ",std_x)
+        print("NEW WSS STD Y: ",std_y)
+        print("NEW WSS STD Z: ",std_z)
+        #data.wss_coord = (data.wss_coord - mean) / ( (maxm.max() - minm.min())/2.)
+        #data.pos_x=((data.pos_x - minm[0]) / ( (maxm[0] - minm[0])))
+        #data.pos_y=torch.tensor(np.expand_dims(data.pos[:,1].detach().numpy(),axis=-1))
+        #data.pos_z=torch.tensor(np.expand_dims(data.pos[:,2].detach().numpy(),axis=-1))
+        #print(data.pos_x.size())
+        print("NEW WSS MAX: ",data.wss_coord.max(dim=-2).values)
+        print("NEW WSS MIN: ",data.wss_coord.min(dim=-2).values)
+        return data
+class Normilize_Norm(object):
+    def __call__(self,data):
+        
+        maxm = data.norm.max(dim=-2).values
+        minm = data.norm.min(dim=-2).values
+        
+        #data.wss_min[:]=minm.min()
+        print("OLD NORM MAX: ",maxm)
+        print("OLD NORM MIN: ",minm)
+        # print("OLD POS_X MAX: ",data.pos_x.max())
+        # print("OLD POS_X MIN: ",data.pos_x.min())
+        #mean = ( maxm.max() + minm.min() ) / 2.
+        #maxm_abs = data.norm.abs().max(dim=-2).values
+        data.norm_max[:]=maxm.max()
+        data.norm_min[:]=minm.min()
+        #data.wss_coord = (data.wss_coord)/maxm_abs.max()
+        data.norm = (data.norm - minm.min()) / ( (maxm.max() - minm.min()))
+        #data.wss_coord = (data.wss_coord - mean) / ( (maxm.max() - minm.min())/2.)
+        #data.pos_x=((data.pos_x - minm[0]) / ( (maxm[0] - minm[0])))
+        #data.pos_y=torch.tensor(np.expand_dims(data.pos[:,1].detach().numpy(),axis=-1))
+        #data.pos_z=torch.tensor(np.expand_dims(data.pos[:,2].detach().numpy(),axis=-1))
+        #print(data.pos_x.size())
+        print("NEW NORM MAX: ",data.norm.max(dim=-2).values)
+        print("NEW NORM MIN: ",data.norm.min(dim=-2).values)
+        return data
+    
+    def __repr__(self):
+        return '{}()'.format(self.__class__.__name__)
+
+class Normalize_vertx(object):
+    def __call__(self,data):
+        maxm = data.pos.max(dim=-2).values
+        minm = data.pos.min(dim=-2).values
+        
+
+        #data.vrtx_max[0,:]=maxm[:]
+        #data.vrtx_min[0,:]=minm[:]
+        print("OLD VERTX MAX: ",maxm)
+        print("OLD VERTX MIN: ",minm)
+        # print("OLD POS_X MAX: ",data.pos_x.max())
+        # print("OLD POS_X MIN: ",data.pos_x.min())
+        
+        #NORMALIZE [-1,1]
+        maxm_abs = data.pos.abs().max(dim=-2).values
+        #data.pos =data.pos/maxm_abs.max()
+        
+        #NORMALIZE [0,1]
+        #data.pos=(data.pos- minm.min())/ (maxm.max() - minm.min())
+        
+        ##LOAD TRAINING MEAN AND STD
+        dataset=MyOwnDataset_normalize_train(root='dataset/training',)
+        train_loader = DataLoader(dataset, batch_size=1)
+        for batch in train_loader:
+            
+            std_x=batch.std_x
+            std_y=batch.std_y
+            std_z=batch.std_z
+            mean_x = batch.mean_x
+            mean_y = batch.mean_y
+            mean_z = batch.mean_z
+            break
+        
+        data.mean_x[:]=mean_x
+        data.mean_y[:]=mean_y
+        data.mean_z[:]=mean_z
+        
+        data.std_x[:]=std_x
+        data.std_y[:]=std_y
+        data.std_z[:]=std_z
+        # print("MEAN X: ",mean_x)
+        # print("MEAN Y: ",mean_y)
+        # print("MEAN Z: ",mean_z)
+        print("MEAN: ",mean_x,mean_y,mean_z)
+        print("STD X: ",std_x)
+        print("STD Y: ",std_y)
+        print("STD Z: ",std_z)
+        # NORMALIZE MEAN=0 STD=1
+        data.pos[:,0] = (data.pos[:,0]-mean_x) /std_x
+        data.pos[:,1] = (data.pos[:,1]-mean_y) /std_y
+        data.pos[:,2] = (data.pos[:,2]-mean_z) /std_z
+        ##
+        #data.pos = (data.pos - mean) / ( (maxm - minm)/2)
+        #data.pos =data.pos/maxm.max()
+        #data.pos=(data.pos- minm.min())/ (maxm.max() - minm.min())
+        #data.pos_x=((data.pos_x - minm[0]) / ( (maxm[0] - minm[0])))
+        #data.pos_y=torch.tensor(np.expand_dims(data.pos[:,1].detach().numpy(),axis=-1))
+        #data.pos_z=torch.tensor(np.expand_dims(data.pos[:,2].detach().numpy(),axis=-1))
+        #print(data.pos_x.size())
+        # mean_x = torch.mean(data.pos[:,0])
+        # mean_y = torch.mean(data.pos[:,1])
+        # mean_z = torch.mean(data.pos[:,2])
+        maxm = data.pos.max(dim=-2).values
+        minm = data.pos.min(dim=-2).values
+        mean = ( maxm + minm ) / 2.
+        
+        std_x=torch.std(data.pos[:,0])
+        std_y=torch.std(data.pos[:,1])
+        std_z=torch.std(data.pos[:,2])
+        # print("NEW MEAN X: ",mean_x)
+        # print("NEW MEAN Y: ",mean_y)
+        # print("NEW MEAN Z: ",mean_z)
+        print("NEW MEAN: ",mean)
+        print("NEW STD X: ",std_x)
+        print("NEW STD Y: ",std_y)
+        print("NEW STD Z: ",std_z)
+        print("NEW VRTX MAX: ",data.pos.max(dim=-2).values)
+        print("NEW VRTX MIN: ",data.pos.min(dim=-2).values)
+        
+        # print("NEW POS_X MAX: ",data.pos_x.max())
+        # print("NEW POS_X MIN: ",data.pos_x.min())
+        return data
+    def __repr__(self):
+        return '{}()'.format(self.__class__.__name__)
+    
+
+#p_trans= [Normalize_vertx(),Normilize_Norm(),]
+p_trans= [Normalize_vertx(),Normilize_WSS()]
+pre_trans=Compose(p_trans)
+
+trans=[
+       RandomRotate(90,axis=0),
+       RandomRotate(90,axis=2),
+       RandomRotate(90,axis=1),
+       ]
+
+pos_trans=Compose(trans)
+pos_trans_0=RandomRotate(90,axis=0)
+pos_trans_1=RandomRotate(90,axis=1)
+pos_trans_2=RandomRotate(90,axis=2)
+
+class MyOwnDataset_normalize_val(InMemoryDataset):
+     def __init__(self, 
+                  root, 
+                  #transform=pos_trans,
+                  transform=None,
+                  pre_transform=pre_trans):
+                  #pre_transform=None):    
+        super(MyOwnDataset_normalize_val, self).__init__(root, transform, pre_transform)
+        self.data, self.slices = torch.load(self.processed_paths[0])
+        #self.transform=transform
+        self.pre_transform=pre_transform
+
+     @property
+     def raw_file_names(self):
+         file_name=glob.glob(os.path.join(self.root,'*.vtp'))
+         return file_name
+
+     @property
+     def processed_file_names(self):
+        return ['data.pt']
+
+
+     def process(self):
+        # Read data into huge `Data` list.
+        res_aug = input("Do you wanna augment the current VALIDATION data? [y,n]:\n")
+        if res_aug=='y':
+            num_aug = int(input("How much rotation per mesh?\n"))
+        data_list = []
+        f2e=FaceToEdge(remove_faces=(False))
+        norm_calculate=GenerateMeshNormals()
+        knn_g=KNNGraph(k=12)
+        for ii,name in enumerate(self.raw_file_names):
+            # print(name)
+            mesh=pv.read(name)
+            
+            faces=mesh.faces.reshape((-1,4))[:, 1:4].T
+            pos=torch.tensor(mesh.points,dtype=torch.float)
+            vrtx_max=pos.max(dim=-2).values
+            vrtx_min=pos.min(dim=-2).values
+            pos=pos-((vrtx_max+vrtx_min)/2.)
+            
+            faces=torch.LongTensor(faces)
+            #wss=torch.tensor(np.expand_dims(mesh.point_arrays["WSS magnitude"],axis=-1),dtype=torch.float)
+            # wss_x=torch.tensor(np.expand_dims(mesh.point_arrays["wss_x"],axis=-1),dtype=torch.float)
+            # wss_y=torch.tensor(np.expand_dims(mesh.point_arrays["wss_y"],axis=-1),dtype=torch.float)
+            # wss_z=torch.tensor(np.expand_dims(mesh.point_arrays["wss_z"],axis=-1),dtype=torch.float)
+            wss=mesh.point_arrays["wss"]
+            #print(wss.size())
+            wss_abs=torch.tensor(np.expand_dims(np.sqrt(wss[:,0]**2+wss[:,1]**2+wss[:,2]**2),axis=-1),dtype=torch.float)
+            #wss=torch.tensor(np.expand_dims(mesh.point_arrays["wss"][:,0],axis=-1),dtype=torch.float)
+            #wss=torch.tensor(mesh.point_arrays["wss"],dtype=torch.float)
+            #print(wss.size())
+            
+            #wss_coord=torch.cat([wss,wss_abs],dim=1)
+            
+            wss_coord=torch.tensor(mesh.point_arrays["wss"],dtype=torch.float)
+            norm=torch.tensor(mesh.point_arrays["norm"],dtype=torch.float)
+            # wss_max=torch.zeros((1,4))
+            # wss_min=torch.zeros((1,4))
+            # vrtx_max=torch.zeros((1,3))
+            # vrtx_min=torch.zeros((1,3))
+            #print(wss.size(0))
+            ##
+            # prova=np.zeros((pos.size(0),1))
+            # prova[:int(pos.size(0)/2)]=0.5
+            
+            # prova=torch.tensor(prova)
+            
+            ##
+            data=Data(
+                pos=pos,
+                #pos_x=pos_x,
+                # pos_y=None,
+                # pos_z=None,
+                face=faces,
+                #wss=wss,
+                # wss_x=wss_x,
+                # wss_y=wss_y,
+                # wss_z=wss_z,
+                wss_coord=wss_coord,
+                wss_abs=wss_abs,
+                wss_max_abs=0.,
+                wss_max=0.,
+                wss_min=0.,
+                wss_std_abs=0.,
+                wss_mean_abs=0.,
+                wss_std_x=0.,
+                wss_std_y=0.,
+                wss_std_z=0.,
+                wss_mean_x=0.,
+                wss_mean_y=0.,
+                wss_mean_z=0.,
+                norm_max=0.,
+                norm_min=0.,
+                norm=norm,
+                std_x=0.,
+                std_y=0.,
+                std_z=0.,
+                mean_x=0.,
+                mean_y=0.,
+                mean_z=0.,
+                # vrtx_max=vrtx_max,
+                # vrtx_min=vrtx_min,
+                # wss_abs=wss_abs,
+                #prova=prova,
+                )
+            #data=knn_g(data)
+            data=f2e(data)
+            #data=m.to('cpu')
+            # nodes=data.pos.numpy()
+            # #
+            # cells=data.face.numpy()
+            # temp=np.array([3]*cells.shape[1])
+            # cells=np.c_[temp,cells.T].ravel()
+            # mesh=pv.PolyData(nodes,cells)
+            # mesh.point_arrays['norm']=data.norm.numpy()
+            # mesh.point_arrays['wss']=data.wss_coord.numpy()
+            # mesh.save(self.root+'/rotated_001/aorta_'+str(ii)+'.vtp')
+            
+            # data_aug_1=pos_trans(data)
+            # data_aug_2=pos_trans(data)
+            # nodes_a=data_aug_1.pos.numpy()
+            # #
+            # cells_a=data_aug_1.face.numpy()
+            # temp_a=np.array([3]*cells_a.shape[1])
+            # cells_a=np.c_[temp_a,cells_a.T].ravel()
+            # mesh_a=pv.PolyData(nodes_a,cells_a)
+            
+            # mesh_a.point_arrays['norm']=data_aug_1.norm.numpy()
+            # mesh_a.point_arrays['wss']=data_aug_1.wss_coord.numpy()
+            # mesh_a.save(self.root+'/rotated_001/rot_aorta_'+str(ii)+'.vtp')
+            #data=norm(data)
+            #data_aug=norm_calculate(data_aug)
+            data_aug=data
+            if res_aug=='y':
+                
+                for augm in range(num_aug):
+                    data_aug=pos_trans(data)
+                    data_list.append(data_aug)
+            
+            data_list.append(data)
+		#data_list.append(data)
+        #print("DATASET COMPLETED \nCURRENT TRAINING MESHES: ",(ii+1)*num_aug )
+            #data_list.append(data)
+            
+            # data_list.append(data_aug_1)
+            # data_list.append(data_aug_2)
+            
+        if self.pre_filter is not None:
+            data_list = [data for data in data_list if self.pre_filter(data)]
+
+        if self.pre_transform is not None:
+            #data_list = [self.pre_transform(data) for data in data_list]
+            data, slices = self.collate(data_list)
+            data=self.pre_transform(data)
+        else:
+            data, slices = self.collate(data_list)
+        
+        torch.save((data, slices), self.processed_paths[0])
+
+
+#dataset=MyOwnDataset_normalize_val(root='dataset/val',)
---
+++ code_for_results/results_normalize.py
@@ -0,0 +1,267 @@
+import torch 
+import matplotlib.pyplot as plt
+#from split_dataset import split_dataset
+import numpy as np
+#from model import GCN
+import pyvista as pv
+
+from losses import nmse,NMAE,Cos_sim,mre
+def relative_error(out,target):
+    
+    r_err=np.zeros((len(out),1),dtype=float)
+    for i in range(len(out)):
+        #print("OUT ",out[i])
+        #print("TARGET",target[i])
+        r_err[i]=(out[i]-target[i])**2
+        #print("ERRORE QUADRO",r_err[i])
+        r_err[i]=np.sqrt(r_err[i])
+        #print("ERRORE RADICE",r_err[i])
+        r_err[i]/=np.sqrt(target[i]**2)
+        #print("ERRORE NORMALIZZATO",r_err[i])
+        # if r_err[i]>10:
+        #     print("ERROR BIGGER THAN 1")
+        #     print("OUT ",out[i])
+        #     print("TARGET",target[i])
+            
+    return r_err
+def denormalize_min_max_wss(point_array,maxm,minm):
+    #maxm=point_array.max()
+    #minm=point_array.min()
+    # print("OLD MAX: ",maxm)
+    # print("OLD MIN: ",minm)
+    #print(maxm)
+    maxm=maxm[0].detach().numpy()
+    minm=minm[0].detach().numpy()
+    
+    new_array=((point_array)*(maxm-minm))+minm
+    # print("NEW MAX: ",new_array.max())
+    # print("NEW MIN: ",new_array.min())
+    return np.expand_dims(new_array,axis=-1)
+
+def denormalize_std_1_wss(point_array,std,mean):
+    std=std[0].detach().numpy()
+    
+    mean=mean.detach().numpy()
+    new_point_array=(point_array*std)+mean
+    return np.expand_dims(new_point_array,axis=-1)
+
+def denormalize_max_abs_wss(point_array,maxm):
+    #maxm=point_array.max()
+    #minm=point_array.min()
+    # print("OLD MAX: ",maxm)
+    # print("OLD MIN: ",minm)
+    #print(maxm)
+    maxm=maxm[0].detach().numpy()
+    #minm=minm[0].detach().numpy()
+    
+    new_array=(point_array)*maxm
+    # print("NEW MAX: ",new_array.max())
+    # print("NEW MIN: ",new_array.min())
+    return np.expand_dims(new_array,axis=-1)
+
+
+"""
+This function use the model given to make a prediction on a mesh,
+'known' is a bool, if True it means that the  original wss values are known, so
+the prediction and the original can be compared
+"""
+
+    
+def predict_on_dataloader(mesh_path,model,data_loaders):
+    model.eval()
+    f= open(mesh_path+'/errors.txt','w+')
+    note=input("NOTE:\n")
+    f.write("NOTE:\n")
+    f.write(note+'\n')        
+    for idx,m in enumerate(data_loaders['val']):
+        
+        #if idx==0:
+            # if m.wss_max[0,0]!=0:
+            #     wss_maxm=m.wss_max
+            #     wss_minm=m.wss_min
+            #     vrtx_maxm=m.vrtx_max
+            #     vrtx_minm=m.vrtx_min
+            
+            out=model(m)
+            f.write("MESH : %d \n" %idx)
+            f.write("NMSE: %f \n" %nmse(out, m.wss_coord).cpu().detach().numpy())
+            f.write("NMAE: %f \n" %np.sum(NMAE(out, m.wss_coord).cpu().detach().numpy()))
+            f.write("COSINE SIMILARITY: %f \n" %Cos_sim(out, m.wss_coord).cpu().detach().numpy())
+            f.write("MRE: %f \n" %mre(out, m.wss_coord).cpu().detach().numpy())
+            print("NMSE: ",nmse(out, m.wss_coord).cpu().detach().numpy())
+            print("NMAE: ",np.sum(NMAE(out, m.wss_coord).cpu().detach().numpy()))
+            print("COSINE SIMILARITY: ",Cos_sim(out, m.wss_coord).cpu().detach().numpy())
+            print("MRE: ",mre(out, m.wss_coord).cpu().detach().numpy())
+            # a=torch.sqrt(out[:,0]**2+out[:,1]**2+out[:,2]**2).unsqueeze(1)
+            # fig, ax = plt.subplots()
+            # ax.plot(m.wss[:,3].cpu(),label='Real')
+            # ax.plot(a.cpu().detach().numpy(),label='Pred')
+            # ax.legend()
+            # #ax.title('One Val sample')
+            # ax.set_xlabel('Vertx')
+            # ax.set_ylabel('WSS_ABS normalized')
+            plt.show()
+            plt.savefig(mesh_path+'/MESH_' +str(idx)+'_y.png')
+            fig, ax = plt.subplots()
+            ax.plot(m.wss_abs.cpu(),label='Real')
+            ax.plot(out.cpu().detach().numpy(),label='Pred')
+            ax.legend()
+            #ax.title('One Val sample')
+            ax.set_xlabel('Vertx')
+            ax.set_ylabel('MESH '+str(idx)+' WSS_ABS normalized')
+            plt.show()
+            plt.savefig(mesh_path+'/MESH_' +str(idx)+'_abs.png')
+            
+            # fig, ax = plt.subplots()
+            # ax.plot(m.wss_coord[:,3].cpu(),label='Real')
+            # ax.plot(out[:,3].cpu().detach().numpy(),label='Pred')
+            # ax.legend()
+            # #ax.title('One Val sample')
+            # ax.set_xlabel('Vertx')
+            # ax.set_ylabel('WSS_abs normalized')
+            # plt.show()
+            #creating the predicted mesh
+            data=m.to('cpu')
+            nodes=data.pos.numpy()
+            # WHEN POS NORMALIZED STD=1
+            nodes[:,0]=nodes[:,0]*data.std_x.numpy()
+            nodes[:,1]=nodes[:,1]*data.std_y.numpy()
+            nodes[:,2]=nodes[:,2]*data.std_z.numpy()
+            #
+            cells=data.face.numpy()
+            temp=np.array([3]*cells.shape[1])
+            cells=np.c_[temp,cells.T].ravel()
+            mesh=pv.PolyData(nodes,cells)
+            # print("MAX: ",m.wss_max)
+            # print("MIN: ",m.wss_min)
+            mesh.point_arrays["norm"]=data.norm.numpy()
+            # wss_x_p=denormalize_max_abs_wss(out[:,0].cpu().detach().numpy(),m.wss_max_abs.cpu())
+            # wss_y_p=denormalize_max_abs_wss(out[:,1].cpu().detach().numpy(),m.wss_max_abs.cpu())
+            # wss_z_p=denormalize_max_abs_wss(out[:,2].cpu().detach().numpy(),m.wss_max_abs.cpu())
+            wss_abs_p=denormalize_std_1_wss(out.cpu().detach().numpy(),m.wss_std_abs.cpu(),m.wss_mean_abs.cpu())
+            mesh.point_arrays["wss_abs_pred"]=wss_abs_p
+            
+            # wss_x=denormalize_max_abs_wss(m.wss_coord[:,0].cpu().detach().numpy(),m.wss_max_abs.cpu())
+            # wss_y=denormalize_max_abs_wss(m.wss_coord[:,1].cpu().detach().numpy(),m.wss_max_abs.cpu())
+            # wss_z=denormalize_max_abs_wss(m.wss_coord[:,2].cpu().detach().numpy(),m.wss_max_abs.cpu())
+            wss_abs=denormalize_std_1_wss(m.wss_abs.cpu().detach().numpy(),m.wss_std_abs.cpu(),m.wss_mean_abs.cpu())
+            mesh.point_arrays["wss_abs"]=wss_abs
+            # mesh.point_arrays["wss_x_pred"]=wss_x_p
+            # mesh.point_arrays["wss_y_pred"]=wss_y_p
+            # mesh.point_arrays["wss_z_pred"]=wss_z_p
+            # # print(wss_x_p)
+            # print(wss_y_p)
+            # print(wss_z_p)
+            #mesh.point_arrays["wss_pred"]=np.concatenate([wss_x_p,wss_y_p,wss_z_p],1)
+            #mesh.point_arrays["wss_abs_pred"]=np.expand_dims(np.sqrt(wss_x**2+wss_y**2+wss_z**2),axis=-1)
+            # mesh.point_arrays["wss_x"]=denormalize_max_abs_wss(m.wss_coord[:,0].cpu().detach().numpy(),m.wss_max.cpu())
+            # mesh.point_arrays["wss_y"]=denormalize_max_abs_wss(m.wss_coord[:,1].cpu().detach().numpy(),m.wss_max.cpu())
+            # mesh.point_arrays["wss_z"]=denormalize_max_abs_wss(m.wss_coord[:,2].cpu().detach().numpy(),m.wss_max.cpu())
+            # #mesh.point_arrays["wss_abs"]=m.wss_abs.cpu().detach().numpy()
+            # wss_x=denormalize_wss(m.wss_coord[:,0].cpu().detach().numpy(),m.wss_max.cpu(),m.wss_min.cpu())
+            # wss_y=denormalize_wss(m.wss_coord[:,1].cpu().detach().numpy(),m.wss_max.cpu(),m.wss_min.cpu())
+            # wss_z=denormalize_wss(m.wss_coord[:,2].cpu().detach().numpy(),m.wss_max.cpu(),m.wss_min.cpu())
+            # mesh.point_arrays["wss"]=np.concatenate([wss_x,wss_y,wss_z],1)
+            # ##
+            #value = input("Choose a name for the prediction file:\n")
+            out_name=mesh_path+'/'+'mesh'+str(idx)+'.vtp'
+            
+            
+            ##
+            # X COMPONENT
+            fig, ax = plt.subplots()
+            
+            ax.plot(np.abs(mesh.point_arrays["wss_abs_pred"]-mesh.point_arrays["wss_abs"]))
+            #ax.legend()
+            #ax.title('One Val sample')
+            ax.set_xlabel('Vertx')
+            ax.set_ylabel('MESH' +str(idx)+' |WSS_ABS-WSS_ABS_PRE|')
+            plt.show()
+            plt.savefig(mesh_path+'/MESH_' +str(idx)+'_dabs.png')
+            #ABS
+            # fig, ax = plt.subplots()
+            
+            # ax.plot(np.abs(mesh.point_arrays["wss_abs_pred"]-mesh.point_arrays["wss_abs"]))
+            # #ax.legend()
+            # #ax.title('One Val sample')
+            # ax.set_xlabel('Vertx')
+            # ax.set_ylabel('|WSS_ABS-WSS_ABS_PRED|')
+            # plt.show()
+            #ERRORE PERCHENTUALE
+            # X COMPONENT
+            fig, ax = plt.subplots()
+            #err_x=np.abs((mesh.point_arrays["wss_x_pred"]-mesh.point_arrays["wss_x"])/mesh.point_arrays["wss_x"])*100
+            err_abs=np.abs((mesh.point_arrays["wss_abs_pred"]-mesh.point_arrays["wss_abs"]))/max(abs(mesh.point_arrays["wss_abs"]))
+            #print("err_x.size(0)")
+            ax.plot(err_abs)
+            #ax.legend()
+            #ax.title('One Val sample')
+            ax.set_xlabel('Vertx')
+            ax.set_ylabel('MESH' +str(idx)+'Error WSS_ABS')
+            plt.show()
+            plt.savefig(mesh_path+'/MESH_' +str(idx)+'_eabs.png')
+            # #ABS
+            # fig, ax = plt.subplots()
+            # err_abs=np.abs((mesh.point_arrays["wss_abs_pred"]-mesh.point_arrays["wss_abs"]))/max(abs(mesh.point_arrays["wss_abs"]))
+            # ax.plot(err_abs)
+            # #ax.legend()
+            # #ax.title('One Val sample')
+            # ax.set_xlabel('Vertx')
+            # ax.set_ylabel('% Error WSS_ABS')
+            # plt.show()
+            
+            # mesh.point_arrays["err_z"]=err_z
+            # mesh.point_arrays["err_x"]=err_x
+            # mesh.point_arrays["err_y"]=err_y
+            # print(len(err_x))
+            # print(len(err_y))
+            # print(len(err_z))
+            mesh.point_arrays["err"]=np.expand_dims(err_abs,-1)
+            print("Mean Error ABS: ",np.mean(err_abs))
+            f.write("Mean Error ABS: %f \n" %np.mean(err_abs))
+            #mesh.point_arrays["err_abs"]=err_abs
+            #mesh.point_arrays["err"]=np.concatenate([err_x,err_y,err_z],1)
+            ##
+            # nodes=data.pos.numpy()
+            # cells=data.face.numpy()
+            # temp=np.array([3]*cells.shape[1])
+            # cells=np.c_[temp,cells.T].ravel()
+            # mesh1=pv.PolyData(nodes,cells)
+            # mesh1.point_arrays["norm"]=data.norm.numpy()
+            # mesh1.point_arrays["wss"]=np.concatenate([wss_x,wss_y,wss_z],1)
+            fig, ax = plt.subplots()
+            #err_x=np.abs((mesh.point_arrays["wss_x_pred"]-mesh.point_arrays["wss_x"])/mesh.point_arrays["wss_x"])*100
+            #r_err_x=np.abs((mesh.point_arrays["wss_pred"][:,0]-mesh.point_arrays["wss"][:,0]))/np.abs(mesh.point_arrays["wss"][:,0])
+            r_err_abs=relative_error(mesh.point_arrays["wss_abs_pred"],mesh.point_arrays["wss_abs"])
+            #print("err_x.size(0)")
+            ax.plot(r_err_abs)
+            #ax.legend()
+            #ax.title('One Val sample')
+            ax.set_xlabel('Vertx')
+            ax.set_ylabel('MESH' +str(idx)+' Relative Error WSS_ABS')
+            plt.show()
+            plt.savefig(mesh_path+'/MESH_' +str(idx)+'_reabs.png')
+            # #ABS
+            # fig, ax = plt.subplots()
+            # err_abs=np.abs((mesh.point_arrays["wss_abs_pred"]-mesh.point_arrays["wss_abs"]))/max(abs(mesh.point_arrays["wss_abs"]))
+            # ax.plot(err_abs)
+            # #ax.legend()
+            # #ax.title('One Val sample')
+            # ax.set_xlabel('Vertx')
+            # ax.set_ylabel('% Error WSS_ABS')
+            # plt.show()
+            
+            # mesh.point_arrays["err_z"]=err_z
+            # mesh.point_arrays["err_x"]=err_x
+            # mesh.point_arrays["err_y"]=err_y
+            # print(len(err_x))
+            # print(len(err_y))
+            # print(len(err_z))
+            #print("FIRST VALUE",r_err_x/mesh.n_points)
+            mesh.point_arrays["r_err"]=np.expand_dims(r_err_abs,-1)
+            print("Mean Relative Error ABS: ",np.sum(r_err_abs)/mesh.n_points)
+            f.write("Mean Relative Error ABS: %f \n" %(np.sum(r_err_abs)/mesh.n_points))
+            
+            mesh.save(out_name)
+    f.close()
+            #break
